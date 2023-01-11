bd9dc3123914e1a00a826687ef711c8d0b546fac
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-01-11 11:55:10 -0800

    about component

---
+++ src/Actions/component/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <div className="conatainer">
+        <h4 className="center">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            repudiandae repellat illo magni eligendi cupiditate voluptates eius
+            nam voluptate. Incidunt nihil ullam quae quia officia quaerat,
+            deserunt eligendi explicabo totam?
+          </p>
+        </h4>
+      </div>
+    </div>
+  );
+}
